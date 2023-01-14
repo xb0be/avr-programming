f19281f8feaabe981864b251e06d7efe8a065042
rludvik <robert.ludvik@cosylab.com>
2023-01-14 18:10:51 +0100

    fixed state machine

--- AtmelStudio/Drafts/StateMachineGarageDoor/StateMachineGarageDoor/settings.h
+++ AtmelStudio/Drafts/StateMachineGarageDoor/StateMachineGarageDoor/settings.h
@@ -1,7 +1,7 @@
 #ifndef settings_H
 #define settings_H
 
-//Define input and output registers and ports
+/* Define input and output registers and ports */
 #define INPUT_REG			DDRB	//Register for inputs
 #define OUTPUT_REG			DDRC	//Register for outputs
 #define INPUT_PORT			PORTB	//Port for inputs
@@ -9,36 +9,34 @@
 #define INPUT_PIN			PINB	//Pin for inputs
 #define OUTPUT_PIN			PORTC	//Pin for outputs
 
-//Buttons		 					- ################ INPUTS ################
-//Pins for push buttons to trigger opening or closing the door
+/* Buttons		 						- ################ INPUTS ################ */
+/* Pins for push buttons to trigger opening or closing the door */
 #define OPEN_BTN_PIN		PB0		//Brown wire
 #define CLOSE_BTN_PIN		PB1		//Green wire
 
-//Pins for switches that signal that door was fully closed or fully open
+/* Pins for switches that signal that door was fully closed or fully open */
 #define OPEN_SWITCH_PIN		PB2		//Red wire
 #define CLOSE_SWITCH_PIN	PB3		//White wire
 
-//Emergency push button to stop the motor running
-#define MOTOR_STOP_PIN		PB4
+/* Emergency push button to stop the motor running */
+#define EMERGENCY_BTN_PIN	PB4
 
-/*LEDs, motor, lock					- ################ OUTPUTS ################
-opening, closing	- green and white LEDs on
-open				- green LED on
-closed				- white LED on
-alarm				- red LED on
-RF signal			- blue LED on
-*/
+/* LEDs, motor, lock					- ################ OUTPUTS ################
+ * opening, closing	- green and white LEDs on
+ * open				- green LED on
+ * closed			- white LED on
+ * alarm			- red LED on
+ * RF signal		- blue LED on
+ */
 #define OPEN_LED_PIN		PC0		//Port for Open LED
 #define CLOSE_LED_PIN		PC1		//Port for Close LED
 #define ALARM_LED_PIN		PC2		//Port for Alarm LED
-//#define RF_LED_PIN			PC3		//Port for RF LED
-#define MOTOR_IN1_PIN		PC4		//To motor's IN1
-#define MOTOR_IN2_PIN		PC5		//To motor's IN2
-#define LOCK_PIN			PC6		//Pin for electro magnetic lock (relay for it)
+#define MOTOR_IN1_PIN		PC3		//To motor's IN1
+#define MOTOR_IN2_PIN		PC4		//To motor's IN2
+#define LOCK_PIN			PC5		//Pin for electro magnetic lock (relay or MOSFET)
+//#define RF_LED_PIN			PC6		//Port for RF LED
 
-/*States definition
- * Define all states of the machine
- */
+/* States definition. Define all states of the machine */
 #define CLOSED		1
 #define CLOSING		2
 #define OPEN		3
@@ -50,9 +48,10 @@ RF signal			- blue LED on
 #define THREE		9
 #define IDLE		10
 
-#define BOUNCETIME	30						//Period for bounce in ms
+/* Period for de-bounce in ms */
+#define BOUNCETIME	30
 
-//UART RF settings
+//UART RF settings - WORK IN PROGRESS
 #define BAUDRATE 9600						//set desired baud rate
 #define UBRRVAL ((F_CPU/(BAUDRATE*8UL))-1)	//calculate UBRR value
 ////Define receive parameters
